b5c3391c834154520f31fe01be4c993cd3795b66
Dwian Luisana Parolinda <50125118+Dwian25@users.noreply.github.com>
2019-12-25 09:42:43 +0700

    Perbandingan Kualitas Citra BMP
    Dwian Luisana, Ali Miftahul Ramdan

---
+++ Perbandingan Kualitas Citra BMP/1. Steganografi/Steganografi-DWT/src/metode/saveGambar.java
@@ -0,0 +1,22 @@
+package metode;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+public class saveGambar {
+
+     public boolean menciptakan_gambar(BufferedImage gmbr_bmp, File file, String ext) {
+        try {
+            file.delete(); // menghapus file yang ada dengan nama yang sama
+            ImageIO.write(gmbr_bmp, ext, file);//menciptakan sebuah file sesuai dengan gambar,ekstensi,dan file
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+                    "File tidak berhasil di simpan!", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+
+}
---
+++ Perbandingan Kualitas Citra BMP/1. Steganografi/Steganografi-DWT/src/testjar/Main.java
@@ -0,0 +1,18 @@
+package testjar;
+
+import javax.swing.JOptionPane;
+import viewer.menuUtm;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // JOptionPane.showMessageDialog(null, "oke");
+        menuUtm mn=new menuUtm();
+        mn.setVisible(true);
+    }
+
+}
